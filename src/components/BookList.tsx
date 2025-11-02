fac84f5f8d882eafb9cf363474a45168916b0a4c
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-02 20:26:29 +0000

    feat(book-context): integrate BookContext for dynamic book rendering and add empty state message

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 import { FlatList, Text, View, StyleSheet } from "react-native";
 import Book from "./Book";
-import { books } from "@/data/books";
+import { useBookContext } from "@/context/BookContext";
+import colors from "@/theme/color";
 import { Book as BookType } from "@/types/book";
 
 const BookList = () => {
-  const renderItem = ({ item }: { item: BookType }) => <Book {...item} />;
+  const { books } = useBookContext();
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={books}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
-      />
+      {books.length > 0 ? (
+        <FlatList
+          data={books}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }: { item: BookType }) => <Book {...item} />}
+        />
+      ) : (
+        <Text style={styles.emptyText}>No books added yet...</Text>
+      )}
     </View>
   );
 };
+
+export default BookList;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
   },
+  emptyText: {
+    textAlign: "center",
+    marginVertical: 200,
+    fontSize: 20,
+    color: colors.primaryText,
+  },
 });
-
-export default BookList;
