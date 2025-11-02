63bfe31318644c40de2e1299fc448b8f50996595
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-02 11:05:43 +0000

    fix(typography): fix values

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,54 +1,55 @@
+import { StyleSheet } from "react-native";
 import colors from "./color";
 
-const typography = {
+const typography = StyleSheet.create({
   heading: {
     fontFamily: "Roboto, sans-serif",
-    fontSize: "24px",
+    fontSize: 24,
     fontWeight: 700,
     color: colors.secondaryText,
   },
   subheading: {
     fontFamily: "Roboto, sans-serif",
-    fontSize: "20px",
+    fontSize: 20,
     fontWeight: 600,
     color: colors.primaryText,
   },
   body: {
     fontFamily: "Roboto, sans-serif",
-    fontSize: "16px",
-    fontWeight: 400,
+    fontSize: 16,
+    fontWeight: "400",
     color: colors.primaryText,
   },
   count: {
     fontFamily: "Roboto, sans-serif",
-    fontSize: "28px",
+    fontSize: 28,
     fontWeight: 600,
     color: colors.buttonBackground,
   },
   button: {
     fontFamily: "Roboto, sans-serif",
-    fontSize: "12px",
+    fontSize: 12,
     fontWeight: 300,
     color: colors.secondaryText,
   },
   title: {
     fontFamily: "Roboto, sans-serif",
-    fontSize: "24px",
+    fontSize: 24,
     fontWeight: 600,
     color: colors.secondaryText,
   },
   label: {
     fontFamily: "Roboto, sans-serif",
-    fontSize: "22px",
+    fontSize: 22,
     fontWeight: 300,
     color: colors.tertiaryText,
   },
   read: {
     fontFamily: "Roboto, sans-serif",
-    fontSize: "16px",
+    fontSize: 16,
     fontWeight: 300,
     color: colors.tertiaryText,
   },
-};
+});
 
 export default typography;
